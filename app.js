6a2bc5696dc0ef634190974b03dd7ec1b26e4f77
Oleg <marchello3007@gmail.com>
2022-07-29 09:12:51 +0300

    add slider & modal

--- app.js
+++ app.js
@@ -1,22 +1,82 @@
+//
+//************  products-counter  **********
+//
+
 let userProductsCounter = document.getElementById("products-counter");
 console.log(userProductsCounter);
+// let howMuchProductsNow = document.querySelector("#products-counter");
+// console.log(howMuchProductsNow);
 
 let btnsAddToCart = document.querySelectorAll(".btn-add-to-cart");
 console.log(btnsAddToCart);
 
-let howMuchProductsNow = document.querySelector("#products-counter");
-console.log(howMuchProductsNow);
-
-let a = howMuchProductsNow.textContent;
-console.log(a);
-
-console.log(howMuchProductsNow.textContent);
-let b = howMuchProductsNow.textContent * 4;
-console.log(b);
-
 for (let i = 0; i < btnsAddToCart.length; i++) {
   btnsAddToCart[i].addEventListener("click", function () {
     console.log("clicked");
-    howMuchProductsNow.textContent = +howMuchProductsNow.textContent + 1;
+    // howMuchProductsNow.textContent = +howMuchProductsNow.textContent + 1;
+    userProductsCounter.textContent = +userProductsCounter.textContent + 1;
   });
 }
+
+//
+//************  likes  ************
+//
+let likeBtns = document.querySelectorAll(".like2");
+console.log(likeBtns);
+
+likeBtns.forEach((item) =>
+  item.addEventListener("click", function () {
+    // console.log("lllllliked!!!!");
+    // if (item.classList.contains("liked")) {
+    //   item.classList.remove("liked");
+    //   console.log("yes");
+    // } else {
+    //   item.classList.add("liked");
+    //   console.log("no");
+    // } або через тогл
+    item.classList.toggle("liked");
+  })
+);
+
+// ******** modal *********
+
+let moreDetailsBtns = document.querySelectorAll(".btn-mere-det");
+// console.log(moreDetailsBtns);
+let modal = document.querySelector(".modal");
+// console.log(modal);
+let closedModal = document.querySelector(".btn-close");
+
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+}
+
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+}
+
+moreDetailsBtns.forEach((item) => item.addEventListener("click", openModal));
+
+closedModal.addEventListener("click", closeModal);
+
+// ********* slider **********
+
+$(".slider-block").slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  dots: true,
+});
+
+// ******** modal when scrolling *********
+//
+//
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("scroll", () => {
+    let scroll = window.scrollY;
+    console.log(scroll);
+    if (scroll > 1100 && scroll < 1250) {
+      openModal();
+    }
+  });
+});
