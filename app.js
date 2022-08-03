57968c1f12716037639590f9a26619dc21259b49
Oleg <marchello3007@gmail.com>
2022-08-03 12:43:32 +0300

    add product-quantity

--- app.js
+++ app.js
@@ -68,15 +68,86 @@ $(".slider-block").slick({
   dots: true,
 });
 
-// ******** modal when scrolling *********
+// ***********  closeModal   **************
+
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
+
+// ******** modal when scrolling (old) *********
 //
 //
-document.addEventListener("DOMContentLoaded", () => {
-  window.addEventListener("scroll", () => {
-    let scroll = window.scrollY;
-    console.log(scroll);
-    if (scroll > 1100 && scroll < 1250) {
-      openModal();
+// document.addEventListener("DOMContentLoaded", () => {
+//   window.addEventListener("scroll", () => {
+//     let scroll = window.scrollY;
+//     console.log(scroll);
+//     if (scroll > 1100 && scroll < 1250) {
+//       openModal();
+//     }
+//   });
+// });
+
+//
+// ******** modal when scrolling (new) *********
+//
+//
+function showModalByScroll() {
+  if (window.pageYOffset > document.body.scrollHeight / 2) {
+    openModal();
+    window.removeEventListener("scroll", showModalByScroll);
+  }
+}
+
+window.addEventListener("scroll", showModalByScroll);
+
+//
+// ******  modal with Time out **********
+
+// setTimeout(() => openModal(), 5000);
+
+//
+//
+//
+// ******  ініціалізація бібліотеки AOS **********
+AOS.init();
+
+//
+//
+//
+// *******  Зміна кількості товарів  ***********
+
+const btns = document.querySelectorAll(".counter_btn");
+// console.log(btns);
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    let direction = this.dataset.direction;
+    let inp = this.parentElement.querySelector(".counter_value");
+    let currentValue = +inp.value;
+    let newValue;
+
+    if (currentValue >= 5 && direction === "plus") {
+      newValue = currentValue + 0;
+    } else {
+      if (direction === "plus") {
+        newValue = currentValue + 1;
+      } else {
+        if (currentValue > 1) {
+          newValue = currentValue - 1;
+        } else {
+          newValue = currentValue - 0;
+        }
+      }
     }
+
+    inp.value = newValue;
+
+    // console.log(btn);
+    // console.log(direction);
+    // console.log(inp);
+    // console.log(currentValue);
+    // console.log(newValue);
   });
 });
